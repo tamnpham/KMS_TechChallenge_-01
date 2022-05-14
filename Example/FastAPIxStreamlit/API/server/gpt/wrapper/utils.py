1423dd5998bf7d25dd20fc17b6da74d738398188
tamnpham <tamnpham@kms-technology.com>
2022-05-14 23:38:05 +0700

    update

--- Example/FastAPIxStreamlit/API/server/gpt/wrapper/utils.py
+++
@@ -1,5 +0,0 @@
-def default_preprocessing(schema: str, question: str) -> str:
-    return f"schema: {schema} | question: {question} | sql:"
-
-def default_posprocessing(result: str) -> str:
-    return result.split('|')[0]
